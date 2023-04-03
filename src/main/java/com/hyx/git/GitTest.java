9992f3d789f791b25a43180505014ca84ea1a972
err <you@example.com>
2023-04-03 20:59:54 +0800

    hot-fix3 commit

--- src/main/java/com/hyx/git/GitTest.java
+++ src/main/java/com/hyx/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello");
         System.out.println("hello");
         System.out.println("hello-hot-fix1");
+        System.out.println("hello-hot-fix3");
     }
 }
