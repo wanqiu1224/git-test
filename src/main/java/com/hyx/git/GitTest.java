cf28eb924d935f1bd93c6818348e74344f338fa6
err <you@example.com>
2023-04-03 20:36:06 +0800

    third commit

--- src/main/java/com/hyx/git/GitTest.java
+++ src/main/java/com/hyx/git/GitTest.java
@@ -7,5 +7,7 @@ public class GitTest {
         System.out.println("hello");
         System.out.println("hello");
         System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
     }
 }
