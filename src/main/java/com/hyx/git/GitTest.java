b972c21a53892fa271632e603ac56c1867c489df
wanqiu1224 <129565521+wanqiu1224@users.noreply.github.com>
2023-04-04 08:46:51 +0800

    Update GitTest.java

--- src/main/java/com/hyx/git/GitTest.java
+++ src/main/java/com/hyx/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("hello-hot-fix3");
         System.out.println("hello-hot-fix2");
         System.out.println("hello-push-fix2");
+        System.out.println("hello-pull-fix2");
     }
 }
